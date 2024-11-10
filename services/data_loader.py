21554f5d10a66a8b9fa8084d1223476b9b1f3a59
Willian Hayashida <will.hayashida07@gmail.com>
2024-11-10 10:05:05 +0100

    data loader for previous commit

--- services/data_loader.py
+++ services/data_loader.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from components.cytoscape import Elements
+from components.cytoscape import Edge, Elements, Node
 from components.graph import Graph
 
 
@@ -7,8 +7,8 @@ def load_data():
     nodes = pd.read_csv("data/nodes.csv")
     edges = pd.read_csv("data/edges.csv")
 
-    nodes = _node_validator(nodes)
-    edges = _edge_validator(edges)
+    nodes = Node.node_validator(nodes)
+    edges = Edge.edge_validator(edges)
 
     elements = Elements.from_dataframe(nodes, edges)
     return {
@@ -18,18 +18,10 @@ def load_data():
     }
 
 
-def _node_validator(nodes: pd.DataFrame) -> pd.DataFrame:
-    _nodes = nodes.copy()
-    _nodes["label"] = _nodes["id"]
-    _nodes["node_type"] = _nodes["node_type"]
-    _nodes["parent"] = _nodes["parent"]
-    _nodes["location"] = _nodes["location"]
-    return _nodes[["id", "label", "node_type", "parent", "location"]]
+def build_node_structure():
+    nodes = pd.read_csv("data/nodes.csv")
 
-
-def _edge_validator(edges: pd.DataFrame) -> pd.DataFrame:
-    _edges = edges.copy()
-    _edges["id"] = _edges["source"] + "->" + _edges["target"]
-    _edges["source"] = _edges["source"]
-    _edges["target"] = _edges["target"]
-    return _edges[["id", "source", "target"]]
+    # initialize workspaces
+    workspaces = nodes["workspace", "workspace_label"].unique()
+    workspaces = workspaces.rename(columns={"workspace": "id", "workspace_label": "label"})
+    
